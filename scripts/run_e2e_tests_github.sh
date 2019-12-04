8cc5e6a871c796dc08eb390c2fe48a459430ac28
François Hodierne <francois@hodierne.net>
2019-12-04 09:13:24 +0100

    ci: support e2e in GitHub Actions

---
+++ scripts/run_e2e_tests_github.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+if [ "$NODE_ENV" = "circleci" ]; then
+  cd /home/runner/work/opencollective-frontend/opencollective-frontend/opencollective-api
+
+  echo "> Starting maildev server"
+  npx maildev &
+  MAILDEV_PID=$!
+
+  echo "> Starting api server"
+  PG_DATABASE=opencollective_dvl MAILDEV_CLIENT=true npm start &
+  API_PID=$!
+  cd /home/runner/work/opencollective-frontend/opencollective-frontend
+  echo "> Starting frontend server"
+  npm start &
+  FRONTEND_PID=$!
+fi
+
+
+# Set `$CYPRESS_RECORD` to `true` in ENV to activate records
+if [ "$CYPRESS_RECORD" = "true" ]; then
+  CYPRESS_RECORD="--record"
+else
+  CYPRESS_RECORD="--record false"
+fi
+
+# Set `$CYPRESS_VIDEO` to `false` in ENV to de-activate videos recording.
+# See https://docs.cypress.io/guides/references/configuration.html#Videos
+
+# Wait for a service to be up
+function wait_for_service() {
+  echo "> Waiting for $1 to be ready... "
+  while true; do
+    nc -z "$2" "$3"
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -eq 0 ]; then
+      echo "> Application $1 is up!"
+      break
+    fi
+    sleep 1
+  done
+}
+
+echo ""
+wait_for_service MAILDEV 127.0.0.1 1080
+echo ""
+wait_for_service API 127.0.0.1 3000
+echo ""
+wait_for_service Frontend 127.0.0.1 3060
+
+echo ""
+echo "> Running cypress tests"
+npx cypress run ${CYPRESS_RECORD}
+RETURN_CODE=$?
+if [ $RETURN_CODE -ne 0 ]; then
+  echo "Error with cypress e2e tests, exiting"
+  exit 1;
+fi
+echo ""
+
+if [ "$NODE_ENV" = "circleci" ]; then
+  echo "Killing all node processes"
+  kill $MAILDEV_PID;
+  kill $API_PID;
+  kill $FRONTEND_PID;
+  echo "Exiting with code $RETURN_CODE"
+  exit $RETURN_CODE
+fi
